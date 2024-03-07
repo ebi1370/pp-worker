5a9cac11e8b0144a19a261b641fcada19443ba6e
ebi1370 <137594275+ebi1370@users.noreply.github.com>
2024-03-07 12:50:29 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,10 +2,10 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+let userID = '7c2b351c-a076-44f3-b947-b25172cf8999';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
-let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+let proxyIP = '141.145.199.188';
 
 let dohURL = 'https://1.1.1.1/dns-query';
 // (dohURL) list :
